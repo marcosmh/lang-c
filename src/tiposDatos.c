810533125e48ba4f38cfbfb12de1ef3c09897c87
Marcos Magaña Hernández <imarcosmaganah@gmail.com>
2022-08-03 22:34:04 -0500

    tipo de datos en c

---
+++ src/tiposDatos.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main() {
+
+    int a = 1;
+    char b = 'c';
+    float c = 1.5;
+    double d = 1.55555555555;
+    short v = 2;
+    long e = 1222222222222222222;
+    
+
+    printf("el valor de entero es de %i \n",a);
+    printf("el valor de char es de %c \n",b);
+    printf("el valor de char es de %f2 \n",c);
+    printf("el valor de char es de %lf \n",d);
+    printf("el valor de char es de %i \n",v);
+    printf("el valor de char es de %ld \n",e);
+    
+
+    return 0;
+}
